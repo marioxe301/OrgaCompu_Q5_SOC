75d52436c019a72b951bbdc983f30026919a6f88
marioxe301 <mariogflores16@gmail.com>
2019-11-18 16:01:54 -0600

    Warnigs corregidos

--- MIPS32SOC-Part2/verilog/MemDecoder.v
+++ MIPS32SOC-Part2/verilog/MemDecoder.v
@@ -4,11 +4,11 @@ module MemDecoder(
     output reg invalidAddr,
     input enable
 );  
-    wire [31:0]TempValueGlobalP;
-    wire [31:0]TempValueStackP;
+    wire [10:0]TempValueGlobalP;
+    wire [10:0]TempValueStackP;
 
-    assign TempValueGlobalP = virtualAddr >> 2;
-    assign TempValueStackP = virtualAddr >> 2;
+    assign TempValueGlobalP = virtualAddr[10:0] >> 2;
+    assign TempValueStackP = virtualAddr[10:0] >> 2;
 
     //global pointer address = 0x10010000 hasta 0x10010FFF  0 a 1023 se toman 12 bits los 12 primeros
     //stack pointer address =  0x7FFFF000 hasta 0x7FFFFFFF 1024 a 2047 
@@ -16,10 +16,10 @@ module MemDecoder(
     always@(*)begin
         if(enable)begin
             if( virtualAddr >= 32'h10010000 && virtualAddr <= 32'h10010FFF ) begin
-                    physicalAddr =  TempValueGlobalP[10:0];
+                    physicalAddr =  TempValueGlobalP;
                     invalidAddr = 1'b0;
             end else if (virtualAddr >= 32'h7FFFF000 &&  virtualAddr <= 32'h7FFFFFFF) begin
-                    physicalAddr = TempValueStackP[10:0];
+                    physicalAddr = TempValueStackP;
                     invalidAddr = 1'b0;
             end else begin
                     physicalAddr = 11'b0;
--- MIPS32SOC-Part2/verilog/PCDecoder.v
+++ MIPS32SOC-Part2/verilog/PCDecoder.v
@@ -3,13 +3,13 @@ module PCDecoder (
     output [9:0]physicalPC,
     output [0:0]invalidPC
 );
-    wire [31:0] tempPC;
+    wire [9:0] tempPC;
 
     //cable temporal
-    assign tempPC = virtualPC >> 2;
+    assign tempPC = virtualPC[9:0] >> 2;
 
     //descartar los 2 bits mas significativos
-    assign physicalPC = tempPC[9:0]; // daria la direccion fisica de la direccion virtual
+    assign physicalPC = tempPC; // daria la direccion fisica de la direccion virtual
     assign invalidPC = ( virtualPC < 32'h00400000 || virtualPC > 32'h00400FFF ) ; // daria el error si es una direccion invalida
 
 endmodule
